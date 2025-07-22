9e186fddb5345fa86ffb437c8522867c6b8f3e9d
Daniel Lopez <daniellop199731@gmail.com>
2025-07-22 16:31:27 -0500

    Explicacion por cambio de nombre

--- src/main/java/com/bancolombia/arka_javadevops_cleanarchitecture_v3/infrastructure/adapter/out/persistence/repository/MetodoPagoUsuarioJR.java
+++ src/main/java/com/bancolombia/arka_javadevops_cleanarchitecture_v3/infrastructure/adapter/out/persistence/repository/MetodoPagoUsuarioJR.java
@@ -4,6 +4,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.bancolombia.arka_javadevops_cleanarchitecture_v3.infrastructure.adapter.out.persistence.entity.MetodoPagoUsuarioEntity;
 
+//MetodoPagoUsuarioJpaRepository: se guarda el archivo como MetodoPagoUsuarioJR
+//porque fork se puso caprichoso con los nombres largos de los archivos
 public interface MetodoPagoUsuarioJR extends JpaRepository<MetodoPagoUsuarioEntity, Integer> {
 
 }
