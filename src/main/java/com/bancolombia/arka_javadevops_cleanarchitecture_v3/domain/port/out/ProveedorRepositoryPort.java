2f2030120eb662a3b6064fc40d98eb7c0e611d62
Daniel Lopez <daniellop199731@gmail.com>
2025-07-27 17:42:07 -0500

    Contratos de comportamiento
    Se crean las interfaces de contrato de comportamiento para acceso a datos y caso de uso

---
+++ src/main/java/com/bancolombia/arka_javadevops_cleanarchitecture_v3/domain/port/out/ProveedorRepositoryPort.java
@@ -0,0 +1,16 @@
+package com.bancolombia.arka_javadevops_cleanarchitecture_v3.domain.port.out;
+
+import java.util.List;
+import java.util.Optional;
+
+import com.bancolombia.arka_javadevops_cleanarchitecture_v3.domain.model.Proveedor;
+
+public interface ProveedorRepositoryPort {
+
+    List<Proveedor> findAll();
+    Optional<Proveedor> findById();
+    Proveedor save(Proveedor proveedor);
+    void deleteById(int idProveedor);
+    boolean existsById(int idProveedor);
+
+}
