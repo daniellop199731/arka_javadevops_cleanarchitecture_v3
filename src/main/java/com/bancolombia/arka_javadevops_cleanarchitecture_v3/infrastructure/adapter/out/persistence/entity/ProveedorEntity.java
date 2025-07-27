d7863d83089d73f89586485359d326ec62dda6bc
Daniel Lopez <daniellop199731@gmail.com>
2025-07-27 18:30:54 -0500

    Creacion del controlador
    Se crea el controlador para accede a los servicios de proveedor
    Se realizan correcciones en el modelo de entidad

--- src/main/java/com/bancolombia/arka_javadevops_cleanarchitecture_v3/infrastructure/adapter/out/persistence/entity/ProveedorEntity.java
+++ src/main/java/com/bancolombia/arka_javadevops_cleanarchitecture_v3/infrastructure/adapter/out/persistence/entity/ProveedorEntity.java
@@ -1,11 +1,16 @@
 package com.bancolombia.arka_javadevops_cleanarchitecture_v3.infrastructure.adapter.out.persistence.entity;
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "proveedores")
 
 @Data
 @NoArgsConstructor
