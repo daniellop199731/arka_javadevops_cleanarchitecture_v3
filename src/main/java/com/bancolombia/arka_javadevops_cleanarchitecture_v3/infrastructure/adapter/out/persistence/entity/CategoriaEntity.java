04bcbb315c53ae66658371d00312ec3f81b22cea
Daniel Lopez <daniellop199731@gmail.com>
2025-07-28 08:33:03 -0500

    Creacion del modelado
    Se crean clases e interfaces para el modelado de datos de Producto.
    Se modifica ProveedorEntity y CategoriaEntity para incluir relaciones con ProductoEntity

--- src/main/java/com/bancolombia/arka_javadevops_cleanarchitecture_v3/infrastructure/adapter/out/persistence/entity/CategoriaEntity.java
+++ src/main/java/com/bancolombia/arka_javadevops_cleanarchitecture_v3/infrastructure/adapter/out/persistence/entity/CategoriaEntity.java
@@ -1,9 +1,15 @@
 package com.bancolombia.arka_javadevops_cleanarchitecture_v3.infrastructure.adapter.out.persistence.entity;
 
+import java.util.List;
+
+import com.bancolombia.arka_javadevops_cleanarchitecture_v3.domain.model.Producto;
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -21,5 +27,9 @@ public class CategoriaEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer idCategoria;
     private String nombreCategoria;
+
+    @JsonBackReference
+    @OneToMany(mappedBy = "categoriaProducto")
+    private List<Producto> productos;    
 
 }
--- src/main/java/com/bancolombia/arka_javadevops_cleanarchitecture_v3/infrastructure/adapter/out/persistence/entity/ProveedorEntity.java
+++ src/main/java/com/bancolombia/arka_javadevops_cleanarchitecture_v3/infrastructure/adapter/out/persistence/entity/ProveedorEntity.java
@@ -1,9 +1,15 @@
 package com.bancolombia.arka_javadevops_cleanarchitecture_v3.infrastructure.adapter.out.persistence.entity;
 
+import java.util.List;
+
+import com.bancolombia.arka_javadevops_cleanarchitecture_v3.domain.model.Producto;
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -25,5 +31,9 @@ public class ProveedorEntity {
     private String nombreProveedor;
     private String telefonoProveedor;
     private String correoElectronicoProveedor;
+
+    @JsonBackReference
+    @OneToMany(mappedBy = "proveedorProducto")
+    private List<Producto> productos;
 
 }
