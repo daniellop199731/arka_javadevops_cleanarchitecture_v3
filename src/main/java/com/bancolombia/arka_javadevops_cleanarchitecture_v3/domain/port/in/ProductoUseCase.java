f98e6fb5b8acc5e0397ec84f662f35063d2f4d8b
Daniel Lopez <daniellop199731@gmail.com>
2025-08-31 12:12:25 -0500

    Cambio contenido rama main
    por error de version de java no se podia compilar el proyecto, se decide partir de la rama SpringCloud ya que esta si compila y lanza todos los proyectos contenidos. Lo demas que sigue programacion reactiva, Docker y demas se seguira trabajando desde este punto

--- src/main/java/com/bancolombia/arka_javadevops_cleanarchitecture_v3/domain/port/in/ProductoUseCase.java
+++ src/main/java/com/bancolombia/arka_javadevops_cleanarchitecture_v3/domain/port/in/ProductoUseCase.java
@@ -3,10 +3,6 @@ package com.bancolombia.arka_javadevops_cleanarchitecture_v3.domain.port.in;
 import java.util.List;
 
 import com.bancolombia.arka_javadevops_cleanarchitecture_v3.domain.model.Producto;
-import com.bancolombia.arka_javadevops_cleanarchitecture_v3.infrastructure.adapter.in.web.dto.ProductoDto;
-
-import reactor.core.publisher.Flux;
-import reactor.core.publisher.Mono;
 
 public interface ProductoUseCase {
 
@@ -17,8 +13,4 @@ public interface ProductoUseCase {
     boolean deleteProducto(int idProducto);
     Producto increaseStock(int idProducto, int stock);
     Producto decreaseStock(int idProducto, int stock);
-
-    Flux<Producto> reactiveGetAllProductos();
-    Mono<ProductoDto> getProductoByIdReactive(int idProducto);
-    Mono<Double> getPrice(int idProducto);
 }
