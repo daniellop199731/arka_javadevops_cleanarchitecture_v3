b56fd95dba06163675d71742d2087feda23489c9
Daniel Lopez <daniellop199731@gmail.com>
2025-09-08 16:16:22 -0500

    Elimancion de proyectos embebidos

--- src/main/java/com/bancolombia/arka_javadevops_cleanarchitecture_v3/ArkaJavadevopsCleanarchitectureV3Application.java
+++ src/main/java/com/bancolombia/arka_javadevops_cleanarchitecture_v3/ArkaJavadevopsCleanarchitectureV3Application.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+
 public class ArkaJavadevopsCleanarchitectureV3Application {
 
 	public static void main(String[] args) {
